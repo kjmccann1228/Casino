5fa184e16011182cf8ef1c5a28e73c95d371d6fe
Ken Ragonese <ken.ragonese@gmail.com>
2016-10-11 17:59:55 -0400

    remove /src from .gitignore

---
+++ src/main/java/Team3Casino/Casino.java
@@ -0,0 +1,11 @@
+package Team3Casino;
+
+/**
+ * Created by kenragonese on 10/11/16.
+ */
+public class Casino {
+
+    public void init(){
+
+    }
+}
---
+++ src/main/java/Team3Casino/CasinoApplication.java
@@ -0,0 +1,12 @@
+package Team3Casino;
+
+/**
+ * Created by kenragonese on 10/11/16.
+ */
+public class CasinoApplication {
+
+    public static void main(String[] args) {
+        Casino casino = new Casino();
+        casino.init();
+    }
+}
