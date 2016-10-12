6238a33e5b431c064431fa36c905ca6f967167cf
Zac Lee <zaclee@Zac-ZCW-29.local>
2016-10-12 08:44:30 -0400

    playerManager class + methods need to be done

---
+++ src/main/java/Team3Casino/PlayerManager.java
@@ -0,0 +1,28 @@
+package Team3Casino;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*** Created by zaclee on 10/12/16. ***/
+
+public class PlayerManager {
+
+    List<Player> listOfPlayer = new ArrayList<Player>();
+    static int playerCounter; // variable to count num of players made?
+
+
+    void CreatePlayerAndAddToList(String name, String pass) {
+
+    }
+
+    void GetPlayer(String name, String pass) {
+
+    }
+
+    String checkBalance(Player player) {
+
+        return null;
+    }
+
+
+}
