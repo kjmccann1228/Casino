9f640911bfd3f6d7d5b98acbbeaddeca6b8df043
Ken Ragonese <ken.ragonese@gmail.com>
2016-10-12 09:40:29 -0400

    add methods in UML to casino class (methods currently don't do anything)

--- src/main/java/Team3Casino/Casino.java
+++ src/main/java/Team3Casino/Casino.java
@@ -3,9 +3,19 @@ package Team3Casino;
 /**
  * Created by kenragonese on 10/11/16.
  */
-public class Casino {
+class Casino {
 
-    public void run(){
-        System.out.println("HEY I'M A CASINO");
+    void run(){
+        //TODO the method that starts up the casino
+    }
+
+    void selectGame(){
+        //TODO select game menu switch
+    }
+    void homeScreen(){
+        //TODO initial home screen menu
+    }
+    void menuScreen(){
+        //TODO menu screen option switch (update player balance?)
     }
 }
