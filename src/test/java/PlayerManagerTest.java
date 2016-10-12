1f35fcfb15984f9c2fc998884304c91b31750638
johnde02 <devon@MacBook-Pro.local>
2016-10-12 16:02:08 -0400

    playerManager test

---
+++ src/test/java/PlayerManagerTest.java
@@ -0,0 +1,28 @@
+import Team3Casino.PlayerManager;
+import Team3Casino.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by devon on 10/12/16.
+ */
+public class PlayerManagerTest {
+
+    @Test
+    public void createPlayerTest() {
+        PlayerManager playerManager = new PlayerManager();
+        playerManager.createPlayer("Devon", "pizza");
+        int expected = 1;
+        int actual = playerManager.playersinCasino.size();
+        Assert.assertEquals("Arraysize should be 1",expected, actual);
+    }
+
+    @Test
+    public void getPlayerByIdTest() {
+        PlayerManager playerManager = new PlayerManager();
+        Player expected = playerManager.createPlayer("Devon", "pizza");
+        Player actual = playerManager.getPlayerById(1,"pizza");
+        Assert.assertEquals("Player should be the same", expected, actual);
+
+    }
+}
